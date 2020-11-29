de0cd91dd499b0e18f7c479d3da09ffd6166f386
Ben Hong <ben@bencodezen.io>
2020-11-29 08:16:45 -0500

    feat: scaffold docs site

---
+++ docs/src/.vuepress/config.js
@@ -0,0 +1,68 @@
+const { description } = require('../../package');
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'Production-Grade Vue',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description:
+    'Building applications that can scale and grow is more than simply following a series of rules. In this workshop, you will learn proven patterns for building production Vue.js apps while gaining an understanding of why certain patterns exist. Equipped with this, you will feel more confident in choosing what is best for your application.',
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    [
+      'meta',
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }
+    ]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: 'https://github.com/bencodezen/production-grade-vue',
+    editLinks: false,
+    docsDir: 'docs',
+    editLinkText: 'Edit this page',
+    lastUpdated: true,
+    nav: [
+      {
+        text: 'Guide',
+        link: '/guide/'
+      },
+      {
+        text: 'Config',
+        link: '/config/'
+      },
+      {
+        text: 'VuePress',
+        link: 'https://v1.vuepress.vuejs.org'
+      }
+    ],
+    sidebar: {
+      '/guide/': [
+        {
+          title: 'Guide',
+          collapsable: false,
+          children: ['', 'using-vue']
+        }
+      ]
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: ['@vuepress/plugin-back-to-top', '@vuepress/plugin-medium-zoom']
+};
---
+++ docs/src/index.md
@@ -0,0 +1,8 @@
+---
+home: true
+heroImage: https://v1.vuepress.vuejs.org/hero.png
+tagline: Official guide for Production-Grade Vue with Frontend Masters
+actionText: Read the Guide →
+actionLink: /guide/
+footer: Made by Ben Hong with ❤️
+---
