2e49e30f07789277bd1ed9aa9dd0a7cb5c7d5d2e
Ben Hong <ben@bencodezen.io>
2020-11-30 10:21:34 -0500

    feat: add computed styles example

---
+++ src/components/ManagingTernaryStyles.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  props: {
+    showElement: {
+      type: Boolean,
+      default: true
+    }
+  },
+  computed: {
+    titleClassNames() {
+      return showElement ? 'is-visible is-bold' : ''
+    }
+  }
+}
+</script>
+
+<template>
+  <!-- Ternary Statement Inline -->
+  <h1 :class="showElement ? 'is-visible is-bold' : ''">
+    Hello Frontend Masters: Styled with a Ternary Statement!
+  </h1>
+  <!-- Computed Class Names -->
+  <h1 :class="titleClassNames">
+    Hello Frontend Masters: Styled with a Computed Property
+  </h1>
+</template>
+
+<style>
+.is-visible {
+  opacity: 1;
+}
+
+.is-bold {
+  font-weight: bold;
+}
+</style>
