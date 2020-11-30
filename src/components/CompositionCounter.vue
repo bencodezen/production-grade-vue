ab37ea97384c70aef333552b375879d91ace71d5
Ben Hong <ben@bencodezen.io>
2020-11-30 11:04:51 -0500

    feat: add challenge 02

---
+++ src/components/CompositionCounter.vue
@@ -0,0 +1,37 @@
+<script>
+/**
+ * Challenge #2
+ * Create a counter that uses the Composition API to:
+ *   - Keep track of a currentCount
+ *   - Increment the count
+ *   - Change the amount that the count can be incremented by
+ */
+export default {
+  data() {
+    return {
+      currentCount: 0,
+      changeAmount: 1
+    }
+  },
+  methods: {
+    incrementCount() {
+      this.currentCount += this.changeAmount
+    }
+  }
+}
+</script>
+
+<template>
+  <div :class="$style['counter-wrapper']">
+    <p>{{ currentCount }}</p>
+    <button @click="incrementCount">Increment Count</button>
+    <input type="number" v-model="changeAmount" />
+  </div>
+</template>
+
+<style module>
+.counter-wrapper {
+  border: 2px solid green;
+  padding: 1rem;
+}
+</style>
