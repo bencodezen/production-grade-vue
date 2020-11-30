c1604a84e43fa222b82db4095e8abff10bc6e28b
Ben Hong <ben@bencodezen.io>
2020-11-30 10:26:10 -0500

    feat: add solution for render function

--- src/components/DynamicHeading.vue
+++ src/components/DynamicHeading.vue
@@ -1,32 +1,15 @@
-<template>
-  <!-- Challenge: Rewrite the following HTML using a render function -->
-  <h1 v-if="level === 1">
-    Hello Frontend Masters!
-  </h1>
-  <h2 v-else-if="level === 2">
-    Hello Frontend Masters!
-  </h2>
-  <h3 v-else-if="level === 3">
-    Hello Frontend Masters!
-  </h3>
-  <h4 v-else-if="level === 4">
-    Hello Frontend Masters!
-  </h4>
-  <h5 v-else-if="level === 5">
-    Hello Frontend Masters!
-  </h5>
-  <h6 v-else-if="level === 6">
-    Hello Frontend Masters!
-  </h6>
-</template>
-
 <script>
+import { h } from 'vue'
+
 export default {
   props: {
     level: {
       type: Number,
       default: 1
     }
+  },
+  render() {
+    return h(`h${this.level}`, 'Hello Frontend Masters!')
   }
 }
 </script>
